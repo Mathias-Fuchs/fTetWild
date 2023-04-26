7f7452518a33a8fea72ae0502d6bf2452a1bb72e
Mathias Fuchs <mathias@mathiasfuchs.com>
2023-04-26 11:11:45 +0200

    build script fix

--- RR.ps1
+++ RR.ps1
@@ -1,20 +1,20 @@
 
-Push-Location
-
-
-[string] $invaliddir = "$home\source\repos\isInvalid" | Resolve-Path
-[string] $source = "$HOME\source\repos\inactive\fTetWild" | Resolve-Path
-[string] $ip =  "$HOME\source\libraries\ftetwild\bin\"   | Resolve-Path
-[string] $inc     = "$HOME\source\libraries\ftetwild\include\floattetwild\"   | Resolve-Path
-
-Set-Location $source
+$ErrorActionPreference = "Stop"
+[string] $sourcedir = "$HOME\source\repos\inactive\fTetWild" | Resolve-Path
+[string] $invaliddir = "$HOME\source\repos\isInvalid" | Resolve-Path
+[string] $ip = "$HOME\source\libraries\ftetwild\bin\" | Resolve-Path
+[string] $inc = "$HOME\source\libraries\ftetwild\include\floattetwild\" | Resolve-Path
 
 . "$invaliddir\makePowershell64DevPowershell.ps1"
-MSBuild.exe ./build/FloatTetwild.vcxproj /p:Configuration=RelWithDebInfo
+Set-Location $sourcedir
+MSBuild.exe ./build/FloatTetwild.vcxproj /p:Configuration=RelWithDebInfo /p:Platform=x64
+
 Copy-Item -Path build/RelWithDebInfo/FloatTetwild.dll -Destination "$ip" -Verbose
 Copy-Item -Path build/RelWithDebInfo/FloatTetwild.lib -Destination "$ip" -Verbose
 Copy-Item -Path build/RelWithDebInfo/FloatTetwild.pdb -Destination "$ip" -Verbose
 
 Copy-Item -Path src/FloatTetwild.h -Destination "$inc" -Verbose
+Copy-Item -Path src/Types.hpp -Destination "$inc" -Verbose
+Copy-Item -Path src/Parameters.h -Destination "$inc" -Verbose
 Pop-Location
 
